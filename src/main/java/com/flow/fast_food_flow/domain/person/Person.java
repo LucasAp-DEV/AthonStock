579c94fed8b28c4cd09038ce3ded673ca9606f51
LucasAp-DEV <lucasapdev@gmail.com>
2024-06-27 13:37:15 -0300

    1°

---
+++ src/main/java/com/flow/fast_food_flow/domain/person/Person.java
@@ -0,0 +1,46 @@
+package com.flow.fast_food_flow.domain.person;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "person")
+public class Person implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+    private String password;
+    private String name;
+    private String email;
+    private String phone;
+
+    @Enumerated(value = EnumType.STRING)
+    private UserRole role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+}
---
+++ src/test/java/com/flow/fast_food_flow/FastFoodFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flow.fast_food_flow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastFoodFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
